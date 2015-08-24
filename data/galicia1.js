2fd6a0663497fc4789cad06096411b2081f9503a
jlnArubaSmart <hi@jln.bz>
2015-08-24 15:47:55 +0200

    First Commit

---
+++ data/galicia1.js
@@ -0,0 +1,128 @@
+loadRecord([
+
+{
+"image":"http://www.coralgrp.com/wp-content/themes/coral/images/bg-img-03.jpg",
+"display": "Telefónica Open Future_ commits US$200 million for innovation platform",
+"url": "http://pressoffice.telefonica.com/jsp/base.jsp?contenido=/jsp/notasdeprensa/notadetalle.jsp&id=0&origen=portada&idm=eng&pais=1&elem=21391"
+},
+{
+"image":"http://www.ebizlatam.com/data/images/news/categories/cat_129/23061.jpg",
+"display": "Siete de cada diez startups de Wayra reciben nuevas rondas de inversón de terceros",
+"url": "http://www.ebizlatam.com/news/129/ARTICLE/23061/2015-05-17.html"
+},
+{
+"image":"http://www.ebizlatam.com/data/images/news/categories/cat_129/i23061/23061-2.jpg",
+"display": "Telefónica presentó propuestas enfocadas en integrar la vida cotidiana al entorno digital",
+"url": "http://www.ebizlatam.com/news/131/ARTICLE/23063/2015-05-17.html"
+},
+{
+"image":"http://blogthinkbig.com/wp-content/uploads/2015/05/promote-app-for-free-with-tappx-community-barcelona-620x349.jpg",
+"display": "Tappx, la plataforma líder de publicidad cruzada, ya es compatible con Firefox OS",
+"url": "http://blogthinkbig.com/tappx-ya-es-compatible-con-firefox-os/"
+},
+{
+"image":"http://www.diariobae.com/wp-content/uploads/2015/05/BAE140515-014F02-300x193.jpg",
+"display": "Startups tecnológicas locales, bajo la lupa de los inversores",
+"url": "http://www.diariobae.com/notas/72512-startups-tecnologicas-locales-bajo-la-lupa-de-los-inversores.html"
+},
+{
+"image":"http://www.iprofesional.com/adjuntos/jpg/2015/05/418575.jpg",
+"display": "Con el apoyo de Wayra Argentina, SocialTools adquirió Postcron",
+"url": "http://www.iprofesional.com/notas/211626-Con-el-apoyo-de-Wayra-Argentina-SocialTools-adquiri-Postcron"
+},
+{
+"image":"http://www.ciberespacio.com.ve/wp-content/uploads/2013/04/wayra-grupo.jpg",
+"display": "Wayra Venezuela celebra tercer aniversario",
+"url": "http://www.eluniversal.com/universo-empresarial/150510/wayra-celebra-tercer-aniversario"
+},
+{
+"image":"http://wayra.co/wp-content/files_mf/1431131080IncreaseGreenCity.jpg",
+"display": "IncreaseCard – New features",
+"url": "http://wayra.co/news/4569_increase_newfeatures.html"
+},
+{
+"image":"http://static1.actualidadecommerce.com/wp-content/uploads/2015/02/telefonica.jpg",
+"display": "Telefónica abre al mundo su ecosistema de innovación",
+"url": "https://www.larepublica.net/app/cms/www/index.php?pk_articulo=533328439"
+},
+{
+"image":"http://elsemanario.com/wp-content/uploads/2014/05/Wayra-en-la-Campus-Party-M%C3%A9xico-2011.jpg",
+"display": "¿Sabes qué son las aceleradoras de startups? Te presentamos a Wayra",
+"url": "http://www.modoemprendedor.com/sabes-que-son-los-aceleradores-de-startups-te-presentamos-a-wayra"
+},
+{
+"image":"http://www.emprendedores.es/var/em/storage/images/emprendedores/ideas-de-negocio/empresas-utilizan-b2b/empresa-b2b-en-espana/geenapp/1304429-1-esl-ES/geenapp_ampliacion.jpg",
+"display": "ESPAÑA: La 'bolsa' de recomendaciones de 'apps' móviles",
+"url": "http://www.entornointeligente.com/articulo/5835879/ESPANA-La-bolsa-de-recomendaciones-de-apps-moviles-04052015"
+},
+{
+"image":"http://img.europapress.net/fotoweb/fotonoticia_20150424141836_644.jpg",
+"display": "La plataforma Open Future_ apoyará el emprendimiento europeo",
+"url": "https://www.casadomo.com/noticias/la-plataforma-open-future-apoyara-el-emprendimiento-europeo"
+},
+{
+"image":"http://www.diario4v.com/u/fotografias/m/2015/5/5/f620x0-11960_11978_0.jpg",
+"display": "Arranca en México el Foro Económico Mundial para Latinoamérica",
+"url": "http://www.diario4v.com/mundo/latinoamerica/2015/5/5/arranca-mexico-foro-economico-mundial-para-latinoamerica-7853.html"
+},
+{
+"image":"http://i1.wp.com/libretadeapuntes.com/wp-content/uploads/2015/05/ganadores2-copia.jpg?resize=300%2C218",
+"display": "Seis emprendimientos serán acelerados por Wayra Colombia",
+"url": "http://libretadeapuntes.com/archivos/41933"
+},
+{
+"image":"http://www.efeemprende.com/wp-content/blogs.dir/2/files_mf/cache/th_c6eb754c68ed005d52d7d204e8b20ddb_foro87-efe1.jpg",
+"display": "Foro Generación 78 organiza una ponencia sobre innovación, emprendimiento y financiación",
+"url": "http://www.efeemprende.com/noticia/foro-generacion-78-ponencia"
+},
+{
+"image":"http://startup.cat/wp-content/uploads/2015/05/Entrevista-Acamica.jpg",
+"display": "Entrevista a la plataforma de Micro-Aprendizaje Acamica",
+"url": "http://startup.cat/entrevista-a-acamica/"
+},
+{
+"image":"http://pulsosocial.com/wp-content/uploads/2014/08/Viajala.png",
+"display": "Colombiana Viajala recibe 500,000 dólares en financiamiento",
+"url": "http://eleconomista.com.mx/tecnociencia/2015/05/07/colombiana-viajala-recibe-500000-dolares-financiamiento"
+},
+{
+"image":"https://www.larepublica.net/app/cms/www/images/201504282121311.2.jpg",
+"display": "Proyectos tecnológicos de Costa Rica recibirán apoyo de Telefónica",
+"url": "https://www.larepublica.net/app/cms/www/index.php?pk_articulo=533328439"
+},
+{
+"image":"http://www.milenio.com/negocios/emprendedores/convocatoria-abierta-proximo-mayo_MILIMA20150429_0177_11.jpg",
+"display": "Wayra abre su convocatoria para emprendedores",
+"url": "http://www.milenio.com/negocios/Wayra-incubadora_Wayra_0_508749291.html"
+},
+{
+"image":"http://img.europapress.net/fotoweb/fotonoticia_20150424141836_644.jpg",
+"display": "Telefónica y Finlandia firman un acuerdo de apoyo a los emprendedores",
+"url": "http://www.europapress.es/economia/noticia-telefonica-open-future-alia-ministerio-empleo-finlandia-20150424141836.html"
+},
+{
+"image":"http://www.dineroenimagen.com/media/dinero/styles/gallerie/public/images/2015/04/smartphone2_1.jpg?itok=TD095ss7",
+"display": "Factico: la geolocalización puede cambiar la forma en que lees noticias",
+"url": "http://www.dineroenimagen.com/2015-04-29/54755"
+},
+{
+"image":"http://cd00.epimg.net/cincodias/imagenes/2015/04/29/tecnologia/1430320484_320643_1430320663_noticia_normal.jpg",
+"display": "Traductores argentinos que crean puestos de trabajo en España",
+"url": "http://cincodias.com/cincodias/2015/04/29/tecnologia/1430320484_320643.html"
+},
+{
+"image":"",
+"display": "Un total de 124 estudiantes murcianos de ESO y Bachiller aprenden a emprender",
+"url": "http://www.europapress.es/murcia/noticia-total-124-estudiantes-murcianos-eso-bachiller-aprenden-emprender-20150429163101.html"
+},
+{
+"image":"http://www.valenciaplaza.com/bd/imagenes/imagen179415d.jpg",
+"display": "Voicemod, llama por teléfono con la voz de Papa Noel o de Darth Vader",
+"url": "http://www.valenciaplaza.com/ver/154480/voicemod-cambio-voz-tiempo-real-app.html"
+}/*,
+{
+"image":"",
+"display": "",
+"url": ""
+}*/
+]);
